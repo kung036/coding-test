bdd34ff15383923e376d43aa51e54bfc8d832362
kung036 <sinariari3@gmail.com>
2023-03-15 15:59:12 +0900

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str1 = "AFCB";
+        String str2 = "BAFC";
+        String str3 = "BAFCZ";
+        String str4 = "AFD";
+        String str  = "AFC";
+
+        System.out.println(str1.compareTo(str));
+        System.out.println(str2.compareTo(str));
+        System.out.println(str3.compareTo(str));
+        System.out.println(str4.compareTo(str));
+        System.out.println(str3.replace(str, ""));
+
+        System.out.println("AAFCA".matches("[A-F]AFC[A-F]"));
+
     }
 }
