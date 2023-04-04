5fb1ca207e6a97f1f53b1723395a0f7f0cc804ee
kung036 <sinariari3@gmail.com>
2023-04-04 21:34:20 +0900

    프로그래머스 Level2 과제 진행하기

---
+++ src/programmers/Level2_4.java
@@ -0,0 +1,80 @@
+package programmers;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Level2_4 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String[][] plans = new String[][]{
+                {"korean", "11:40", "30"},
+                {"english", "12:10", "20"},
+                {"math", "12:30", "40"}
+        };
+
+        String[] answer = s.solution(plans);
+        System.out.println("answer : " + answer.toString()); // 7
+
+        plans = new String[][]{
+                {"korean", "11:40", "30"},
+                {"english", "12:10", "20"},
+                {"math", "12:30", "40"}
+        };
+        answer = s.solution(plans);
+        System.out.println("answer : " + answer.toString()); // 7
+    }
+
+    static class Solution {
+        public String[] solution(String[][] plans) {
+            List<String> answer = new ArrayList<>();
+            Stack<Integer> s = new Stack<>();
+
+            int[][] study = new int[plans.length][4];
+            Arrays.sort(plans, (s1, s2) -> s1[1].compareTo(s2[1]));
+            Arrays.stream(plans).forEach(strings ->{
+                        Arrays.stream(strings).forEach(s1 -> System.out.print(s1 + " "));
+                        System.out.println();
+                    }
+            );
+            for(int i=0; i<plans.length; i++) {
+                study[i][0] = Integer.valueOf(plans[i][1].substring(0, 2));
+                study[i][1] = Integer.valueOf(plans[i][1].substring(3, 5));
+                study[i][2] = Integer.valueOf(plans[i][2]);
+                study[i][3] = i;
+            }
+
+            // 정렬하기
+            for(int i=0; i<study.length; i++) {
+                if(i != study.length-1) {
+                    int time = (study[i+1][0] - study[i][0]) * 60 + study[i+1][1] - study[i][1];
+                    if(time >= study[i][2]) {
+                        answer.add(plans[study[i][3]][0]);
+
+                        int leftTime = time - study[i][2];
+                        while(leftTime > 0 && !s.isEmpty()) {
+                            int[] tmp = study[s.peek()];
+                            if(leftTime >= tmp[2]) {
+                                answer.add(plans[study[s.pop()][3]][0]);
+                            } else {
+                                tmp[2] -= leftTime;
+                            }
+                            leftTime -= tmp[2];
+                        }
+
+                    } else {
+                        study[i][2] -= time;
+                        s.push(i);
+                    }
+                } else {
+                    answer.add(plans[i][0]);
+                }
+            }
+
+            while(!s.isEmpty()) {
+                answer.add(plans[study[s.pop()][3]][0]);
+            }
+
+            return answer.toArray(new String[answer.size()]);
+        }
+    }
+}
