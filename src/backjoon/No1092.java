7496c44813e8e276ca1bda42e57d6152c4c22d71
kung036 <sinariari3@gmail.com>
2023-05-20 15:47:01 +0900

    백준 1092번 - 배

--- src/backjoon/No1092.java
+++ src/backjoon/No1092.java
@@ -1,12 +1,63 @@
+//package backjoon;
+//
+//import java.io.*;
+//import java.util.*;
+//import java.util.stream.Collectors;
+//
+//public class No1092 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int N = Integer.parseInt(br.readLine()); // 크레인 개수
+//        List<Integer> crane_weigth =
+//                Arrays.stream(br.readLine().split(" "))
+//                        .map(s -> Integer.parseInt(s))
+//                        .sorted(Comparator.reverseOrder())
+//                        .collect(Collectors.toList());  // 크레인별 무게 제한
+//        int M = Integer.parseInt(br.readLine()); // 박스 개수
+//        List<Integer> box_weigth =
+//                Arrays.stream(br.readLine().split(" "))
+//                        .map(s -> Integer.parseInt(s))
+//                        .sorted(Comparator.reverseOrder())
+//                        .collect(Collectors.toList()); // 크레인별 무게 제한
+//
+//        Collections.sort(crane_weigth, Collections.reverseOrder());
+//        Collections.sort(box_weigth, Collections.reverseOrder());
+//
+//        // edge
+//        if(crane_weigth.get(0) < box_weigth.get(0)) {
+//            System.out.println(-1);
+//            return;
+//        }
+//
+//        int result = 0; // 결과
+////        System.out.print("크레인 : ");
+////        Arrays.stream(crane_weigth).forEach(value -> System.out.print(value + " " ));
+////        System.out.print("\n박스 : ");
+////        Arrays.stream(box_weigth).forEach(value -> System.out.print(value + " " ));
+////        System.out.println("\nM : " + M);
+//
+//        while(!box_weigth.isEmpty()) {
+//            int idx = 0;
+//            for(int i=0; i<N;) {
+//                if(idx == box_weigth.size()) break;
+//                if(box_weigth.get(idx) <= crane_weigth.get(i)) {
+//                    box_weigth.remove(idx);
+//                    i++;
+//                } else idx++;
+//            }
+//            result++;
+//        }
+//
+//        System.out.println(result);
+//    }
+//}
+
+// 내가 작성한 코드
 package backjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class No1092 {
     public static void main(String[] args) throws IOException {
@@ -30,16 +81,17 @@ public class No1092 {
 
         int result = 0; // 결과
         boolean[] visited = new boolean[M];
-        System.out.print("크레인 : ");
-        Arrays.stream(crane_weigth).forEach(value -> System.out.print(value + " " ));
-        System.out.print("\n박스 : ");
-        Arrays.stream(box_weigth).forEach(value -> System.out.print(value + " " ));
-        System.out.println("\nM : " + M);
+
+        // edge
+        if(crane_weigth[0] < box_weigth[0]) {
+            System.out.println(-1);
+            return;
+        }
+
 
         int startPoint = 0;
         int checkPoint = 0;
         while(true) {
-            System.out.println("startPoint : " + startPoint + " checkPoint : " + checkPoint);
             startPoint = checkPoint;
             checkPoint = -1;
             for(int i=0; i<N; i++) {
@@ -67,9 +119,7 @@ public class No1092 {
                 }
                 if(startPoint >= M) break;
             }
-            System.out.println("startPoint : " + startPoint + " checkPoint : " + checkPoint);
             result++;
-            System.out.println(result + "\n" + "-".repeat(20));
             if(checkPoint == -1 && startPoint >= M) break;
             if(checkPoint == -1) checkPoint = startPoint;
         }
