67de8ef89c8a8e8e6af8c3f8709a3a15f6a9c150
kung036 <sinariari3@gmail.com>
2023-05-23 15:34:02 +0900

    백준 13164번 - 행복 유치원

--- src/backjoon/No13164.java
+++ src/backjoon/No13164.java
@@ -1,7 +1,40 @@
 package backjoon;
 
-public class No13164 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
+public class No13164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] tmp = br.readLine().split(" ");
+        int kid = Integer.parseInt(tmp[0]);
+        int group = Integer.parseInt(tmp[1]);
+
+        int[] height = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(s -> Integer.parseInt(s))
+                .toArray();
+
+        if(kid == group) {
+            System.out.println(0);
+            return ;
+        }
+
+        int cost = 0;
+
+        List<Integer> list = new ArrayList<>();
+        for(int i=1; i<kid; i++) {
+            list.add(height[i] - height[i-1]);
+        }
+
+        Collections.sort(list);
+
+        for(int i=0; i<kid-group; i++) {
+            cost += list.get(i);
+        }
+
+        System.out.println(cost);
     }
 }
