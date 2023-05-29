777774ffe6cd99d93e9c7b71854bb435c90b8834
kung036 <sinariari3@gmail.com>
2023-05-29 13:47:27 +0900

    백준 2473번 - 세 용액

--- src/backjoon/No2473.java
+++ src/backjoon/No2473.java
@@ -4,49 +4,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class No2473 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] water = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(s -> Integer.parseInt(s))
+        long[] water = Arrays.stream(br.readLine().split(" "))
+                .mapToLong(s -> Long.parseLong(s))
                 .sorted()
                 .toArray();
 
-        int left = 0;
-        int mid = 0;
-        int right = N-1;
+        Long[] ans = { water[0], water[1], water[2] };
+        Long sum = Math.abs(water[0] + water[1] + water[2]);
 
-        for(int i=0; i<N; i++) {
-            if(water[i] > 0) {
-                mid = i;
-                break;
+        for(int i=0; i<N-2; i++) {
+            Long fixed = water[i];
+            int left = i+1;
+            int right = N-1;
+
+            while(left<right) {
+                Long tmp_sum = fixed + water[left] + water[right];
+                if(Math.abs(tmp_sum) < sum) {
+                    ans[0] = fixed;
+                    ans[1] = water[left];
+                    ans[2] = water[right];
+                    sum = Math.abs(tmp_sum);
+                }
+
+                if(0 < tmp_sum) right--;
+                else if(tmp_sum < 0) left++;
+                else {
+                    System.out.println(ans[0] + " " + ans[1] + " " + ans[2]);
+                    return;
+                }
             }
         }
 
-        while(true) {
-            if(water[left] + water[right] > 0) right--;
-            else left++;
-
-            if(water[left]+water[right]+water[mid] == 0) break;
-            else if(left >= right) {
-                if(water[left] >= 0) left--;
-                else right--;
-                break;
-            }
-
-            int before = mid;
-            if(water[left]+water[right]+water[mid] > 0) mid--;
-            else mid++;
-
-            if(left >= mid || mid >= right) {
-                mid = before;
-                break;
-            }
-        }
-
-        System.out.println(water[left] + " " + water[mid] + " " + water[right]);
+        System.out.println(ans[0] + " " + ans[1] + " " + ans[2]);
     }
 }
