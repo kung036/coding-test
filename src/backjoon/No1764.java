c5f79f3d260011664345a97c4aebc8eeeede48e2
kung036 <sinariari3@gmail.com>
2023-06-05 17:02:08 +0900

    백준 1764번 - 듣보잡

---
+++ src/backjoon/No1764.java
@@ -0,0 +1,23 @@
+package backjoon;
+
+import java.util.*;
+
+// 듣보잡
+public class No1764 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        Set<String> set = new HashSet<>();
+        List<String> list = new ArrayList<>();
+        while(N-- > 0) {
+            list.add(sc.next());
+            set.add(sc.next());
+        }
+        while(M-- > 0) {
+//            if(set.contains()) break;
+//            if(list.contains())
+        }
+    }
+}
