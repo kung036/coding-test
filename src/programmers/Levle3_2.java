5b1b8e015472108a33a8df2a7b3c20e5e93c260b
kung036 <sinariari3@gmail.com>
2023-06-20 22:20:19 +0900

    프로그래머스 level3 등산코스 정하기

---
+++ src/programmers/Levle3_2.java
@@ -0,0 +1,112 @@
+package programmers;
+
+import java.util.*;
+
+// 등산코스 정하기
+public class Levle3_2 {
+    public static void main(String[] args) {
+        Node node = new Node(1, 2);
+        Solution s = new Solution();
+        s.solution("");
+    }
+
+    static class Node {
+        int num;
+        int weight;
+
+        Node(int num, int weight) {
+            this.num = num;
+            this.weight = weight;
+        }
+    }
+}
+
+class Solution {
+    List<List<Node>> nums;
+    public int solution(String numbers) {
+        int answer = 0;
+
+        // 그래프 그리기
+        nums = new ArrayList<>();
+        nums_init();
+
+        // 다익스트라 알고리즘
+        answer = dijkstra(numbers);
+
+
+        return answer;
+    }
+
+    public int dijkstra(String num) {
+        int[] numbers = Arrays.stream(num.split(""))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.add()
+
+    }
+
+    public void nums_init() {
+        for(int i=0; i<10; i++) {
+            nums.add(new ArrayList<>());
+        }
+
+        // 0
+        nums.get(0).add(new Node(8, 2));
+        nums.get(0).add(new Node(7, 3));
+        nums.get(0).add(new Node(9, 3));
+        // 1
+        nums.get(1).add(new Node(2, 2));
+        nums.get(1).add(new Node(4, 2));
+        nums.get(1).add(new Node(5, 3));
+        // 2
+        nums.get(2).add(new Node(1, 2));
+        nums.get(2).add(new Node(3, 2));
+        nums.get(2).add(new Node(5, 2));
+        nums.get(2).add(new Node(4, 3));
+        nums.get(2).add(new Node(6, 3));
+        // 3
+        nums.get(3).add(new Node(2, 2));
+        nums.get(3).add(new Node(6, 2));
+        nums.get(3).add(new Node(5, 3));
+        // 4
+        nums.get(4).add(new Node(1, 2));
+        nums.get(4).add(new Node(5, 2));
+        nums.get(4).add(new Node(7, 2));
+        nums.get(4).add(new Node(2, 3));
+        nums.get(4).add(new Node(8, 3));
+        // 5
+        nums.get(5).add(new Node(2, 2));
+        nums.get(5).add(new Node(4, 2));
+        nums.get(5).add(new Node(6, 2));
+        nums.get(5).add(new Node(8, 2));
+        nums.get(5).add(new Node(1, 3));
+        nums.get(5).add(new Node(3, 3));
+        nums.get(5).add(new Node(7, 3));
+        nums.get(5).add(new Node(9, 3));
+        // 6
+        nums.get(6).add(new Node(3, 2));
+        nums.get(6).add(new Node(5, 2));
+        nums.get(6).add(new Node(9, 2));
+        nums.get(6).add(new Node(2, 3));
+        nums.get(6).add(new Node(8, 3));
+        // 7
+        nums.get(7).add(new Node(4, 2));
+        nums.get(7).add(new Node(8, 2));
+        nums.get(7).add(new Node(5, 3));
+        nums.get(7).add(new Node(0, 3));
+        // 8
+        nums.get(8).add(new Node(5, 2));
+        nums.get(8).add(new Node(7, 2));
+        nums.get(8).add(new Node(9, 2));
+        nums.get(8).add(new Node(0, 2));
+        nums.get(8).add(new Node(4, 3));
+        nums.get(8).add(new Node(6, 3));
+        // 9
+        nums.get(9).add(new Node(6, 2));
+        nums.get(9).add(new Node(8, 2));
+        nums.get(9).add(new Node(5, 3));
+        nums.get(9).add(new Node(0, 3));
+    }
+}
