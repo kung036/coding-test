54b641b2e55876e55bd7f01777ed9420cea59fcf
kung036 <sinariari3@gmail.com>
2023-06-23 14:48:33 +0900

    프로그래머스 level3 양과 늑대

--- src/programmers/Levle3_2.java
+++ src/programmers/Levle3_2.java
@@ -19,94 +19,95 @@ public class Levle3_2 {
             this.weight = weight;
         }
     }
-}
 
-class Solution {
-    List<List<Node>> nums;
-    public int solution(String numbers) {
-        int answer = 0;
+    static class Solution {
+        List<List<programmers.Node>> nums;
+        public int solution(String numbers) {
+            int answer = 0;
 
-        // 그래프 그리기
-        nums = new ArrayList<>();
-        nums_init();
+            // 그래프 그리기
+            nums = new ArrayList<>();
+            nums_init();
 
-        // 다익스트라 알고리즘
-        answer = dijkstra(numbers);
+            // 다익스트라 알고리즘
+            answer = dijkstra(numbers);
 
 
-        return answer;
-    }
-
-    public int dijkstra(String num) {
-        int[] numbers = Arrays.stream(num.split(""))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        Queue<Integer> queue = new PriorityQueue<>();
-        queue.add()
-
-    }
-
-    public void nums_init() {
-        for(int i=0; i<10; i++) {
-            nums.add(new ArrayList<>());
+            return answer;
         }
 
-        // 0
-        nums.get(0).add(new Node(8, 2));
-        nums.get(0).add(new Node(7, 3));
-        nums.get(0).add(new Node(9, 3));
-        // 1
-        nums.get(1).add(new Node(2, 2));
-        nums.get(1).add(new Node(4, 2));
-        nums.get(1).add(new Node(5, 3));
-        // 2
-        nums.get(2).add(new Node(1, 2));
-        nums.get(2).add(new Node(3, 2));
-        nums.get(2).add(new Node(5, 2));
-        nums.get(2).add(new Node(4, 3));
-        nums.get(2).add(new Node(6, 3));
-        // 3
-        nums.get(3).add(new Node(2, 2));
-        nums.get(3).add(new Node(6, 2));
-        nums.get(3).add(new Node(5, 3));
-        // 4
-        nums.get(4).add(new Node(1, 2));
-        nums.get(4).add(new Node(5, 2));
-        nums.get(4).add(new Node(7, 2));
-        nums.get(4).add(new Node(2, 3));
-        nums.get(4).add(new Node(8, 3));
-        // 5
-        nums.get(5).add(new Node(2, 2));
-        nums.get(5).add(new Node(4, 2));
-        nums.get(5).add(new Node(6, 2));
-        nums.get(5).add(new Node(8, 2));
-        nums.get(5).add(new Node(1, 3));
-        nums.get(5).add(new Node(3, 3));
-        nums.get(5).add(new Node(7, 3));
-        nums.get(5).add(new Node(9, 3));
-        // 6
-        nums.get(6).add(new Node(3, 2));
-        nums.get(6).add(new Node(5, 2));
-        nums.get(6).add(new Node(9, 2));
-        nums.get(6).add(new Node(2, 3));
-        nums.get(6).add(new Node(8, 3));
-        // 7
-        nums.get(7).add(new Node(4, 2));
-        nums.get(7).add(new Node(8, 2));
-        nums.get(7).add(new Node(5, 3));
-        nums.get(7).add(new Node(0, 3));
-        // 8
-        nums.get(8).add(new Node(5, 2));
-        nums.get(8).add(new Node(7, 2));
-        nums.get(8).add(new Node(9, 2));
-        nums.get(8).add(new Node(0, 2));
-        nums.get(8).add(new Node(4, 3));
-        nums.get(8).add(new Node(6, 3));
-        // 9
-        nums.get(9).add(new Node(6, 2));
-        nums.get(9).add(new Node(8, 2));
-        nums.get(9).add(new Node(5, 3));
-        nums.get(9).add(new Node(0, 3));
+        public int dijkstra(String num) {
+            int[] numbers = Arrays.stream(num.split(""))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            Queue<Integer> queue = new PriorityQueue<>();
+//        queue.add();
+
+            return numbers.length;
+        }
+
+        public void nums_init() {
+            for(int i=0; i<10; i++) {
+                nums.add(new ArrayList<>());
+            }
+
+            // 0
+            nums.get(0).add(new programmers.Node(8, 2));
+            nums.get(0).add(new programmers.Node(7, 3));
+            nums.get(0).add(new programmers.Node(9, 3));
+            // 1
+            nums.get(1).add(new programmers.Node(2, 2));
+            nums.get(1).add(new programmers.Node(4, 2));
+            nums.get(1).add(new programmers.Node(5, 3));
+            // 2
+            nums.get(2).add(new programmers.Node(1, 2));
+            nums.get(2).add(new programmers.Node(3, 2));
+            nums.get(2).add(new programmers.Node(5, 2));
+            nums.get(2).add(new programmers.Node(4, 3));
+            nums.get(2).add(new programmers.Node(6, 3));
+            // 3
+            nums.get(3).add(new programmers.Node(2, 2));
+            nums.get(3).add(new programmers.Node(6, 2));
+            nums.get(3).add(new programmers.Node(5, 3));
+            // 4
+            nums.get(4).add(new programmers.Node(1, 2));
+            nums.get(4).add(new programmers.Node(5, 2));
+            nums.get(4).add(new programmers.Node(7, 2));
+            nums.get(4).add(new programmers.Node(2, 3));
+            nums.get(4).add(new programmers.Node(8, 3));
+            // 5
+            nums.get(5).add(new programmers.Node(2, 2));
+            nums.get(5).add(new programmers.Node(4, 2));
+            nums.get(5).add(new programmers.Node(6, 2));
+            nums.get(5).add(new programmers.Node(8, 2));
+            nums.get(5).add(new programmers.Node(1, 3));
+            nums.get(5).add(new programmers.Node(3, 3));
+            nums.get(5).add(new programmers.Node(7, 3));
+            nums.get(5).add(new programmers.Node(9, 3));
+            // 6
+            nums.get(6).add(new programmers.Node(3, 2));
+            nums.get(6).add(new programmers.Node(5, 2));
+            nums.get(6).add(new programmers.Node(9, 2));
+            nums.get(6).add(new programmers.Node(2, 3));
+            nums.get(6).add(new programmers.Node(8, 3));
+            // 7
+            nums.get(7).add(new programmers.Node(4, 2));
+            nums.get(7).add(new programmers.Node(8, 2));
+            nums.get(7).add(new programmers.Node(5, 3));
+            nums.get(7).add(new programmers.Node(0, 3));
+            // 8
+            nums.get(8).add(new programmers.Node(5, 2));
+            nums.get(8).add(new programmers.Node(7, 2));
+            nums.get(8).add(new programmers.Node(9, 2));
+            nums.get(8).add(new programmers.Node(0, 2));
+            nums.get(8).add(new programmers.Node(4, 3));
+            nums.get(8).add(new programmers.Node(6, 3));
+            // 9
+            nums.get(9).add(new programmers.Node(6, 2));
+            nums.get(9).add(new programmers.Node(8, 2));
+            nums.get(9).add(new programmers.Node(5, 3));
+            nums.get(9).add(new programmers.Node(0, 3));
+        }
     }
 }
