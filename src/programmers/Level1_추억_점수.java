a32f56fdb7936df775098fb6c4b3bd42b3406f5c
kung036 <sinariari3@gmail.com>
2023-07-28 21:10:55 +0900

    프로그래머스 level1 추억 점수

--- src/programmers/Level1_추억_점수.java
+++ src/programmers/Level1_추억_점수.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 // 추억 점수
 class Level1_추억_점수 {
-    public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = new int[name.length];
+    public static int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
 
         // name과 yearning이 매핑된 map 선언
         Map<String, Integer> nameAndYearning = new HashMap<>();
@@ -18,11 +18,21 @@ class Level1_추억_점수 {
         for(String[] names : photo) {
             int sum = 0;
             for(String n : names) {
-                sum += nameAndYearning.get(n);
+                sum += Optional.ofNullable(nameAndYearning.get(n)).orElse(0);
             }
             answer[i++] = sum;
         }
 
         return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] name = {"may", "kein", "kain", "radi"};
+        int[] yearning = {5, 10, 1, 3};
+        String[][] photo = {{"may", "kein", "kain", "radi"},
+                {"may", "kein", "brin", "deny"},
+                {"kon", "kain", "may", "coni"}};
+        int[] answer = solution(name, yearning, photo);
+        System.out.println(Arrays.toString(answer));
     }
 }
