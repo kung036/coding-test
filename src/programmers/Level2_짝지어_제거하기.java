484d951b8121127e5272575e59291835732607bd
kung036 <sinariari3@gmail.com>
2024-01-30 11:21:32 +0900

    프로그래머스 level2 짝지어 제거하기

--- src/programmers/Level2_짝지어_제거하기.java
+++ src/programmers/Level2_짝지어_제거하기.java
@@ -2,50 +2,21 @@ package programmers;
 import java.util.*;
 
 // stack 이용
-// 9, 11, 12 -> 런타임 에러
-// 2, 4, 5, 6, 7 -> 효율성 테스트 실패
+// 참고 사이트 : https://velog.io/@dhk22/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%A7%9D%EC%A7%80%EC%96%B4-%EC%A0%9C%EA%B1%B0%ED%95%98%EA%B8%B0-Java
 
 public class Level2_짝지어_제거하기 {
     public int solution(String s) {
         int answer = 0;
 
-        Stack<String> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
 
-        List<String> list = new ArrayList<>();
-        Collections.addAll(list, s.split(""));
-        int last = list.size();
+        for(int i=0; i<s.length(); i++) {
+            Character c = s.charAt(i);
 
-        int index = 0;
-
-        while(index < last) {
-            System.out.println(index + " " + last);
-            System.out.println(stack.toString());
-            if(stack.isEmpty()) stack.push(list.get(index++));
-
-            String before = stack.peek();
-            stack.push(list.get(index++));
-            String after = stack.peek();
-
-            System.out.println("후1 : " + stack.toString());
-
-            // 같은 문자인 경우
-            if(before.equals(after)) {
-                stack.pop();
-                stack.pop();
-            }
-
-            System.out.println("후2 : " + stack.toString());
-
-            while(stack.size() >= 2 && before.equals(after)) {
-                before = stack.pop();
-                after = stack.peek();
-
-                if(before.equals(after)) { // 같은 문자인 경우
-                    stack.pop();
-                } else { // 다른 문자인 경우
-                    stack.push(before);
-                    break;
-                }
+            if(stack.isEmpty()) stack.push(c);
+            else {
+                if(stack.peek() == c) stack.pop();
+                else stack.push(c);
             }
         }
 
