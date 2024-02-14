961bf176582baee795b3ec46e67b8e920db8b736
kung036 <sinariari3@gmail.com>
2024-02-14 11:49:09 +0900

    프로그래머스 level2 2 x n 타일링

--- src/programmers/Level2_2xn_타일링.java
+++ src/programmers/Level2_2xn_타일링.java
@@ -1,64 +1,19 @@
 package programmers;
 
-// 정확성, 효율성 -> 전부 미통과
+// 조합이 아닌 dp로 문제를 풀어야함
+// https://gom20.tistory.com/65
 
 public class Level2_2xn_타일링 {
-    int[] factorial_arr = new int[60001];
+        public int solution(int n) {
+            int[] dp = new int[60001];
+            dp[1] = 1;
+            dp[2] = 2;
 
-    public int solution(int n) {
-        int answer = 0;
+            for(int i=3; i<=n; i++) {
+                dp[i] = dp[i-1] + dp[i-2];
+                if(dp[i] >= 1000000007) dp[i] %= 1000000007;
+            }
 
-        // 세로(ㅣ)
-        int a = n;
-
-        // 가로(ㅡ)
-        int b = 0;
-
-        while(true) {
-            // 조합 n C b
-            answer += combination(a+b, b);
-
-            if(a > 1) {
-                a-=2;
-                b++;
-            } else break;
-            // else if(n%2 == 0) {
-            //     a-=2;
-            //     b++;
-            // }
+            return dp[n];
         }
-
-        return answer;
     }
-
-    // 팩토리얼
-    public int factorial(int n) {
-        if(n == 1 || n == 0) return 1;
-        return n * factorial(n-1);
-    }
-
-    public int fact(int n) {
-        if(factorial_arr[n] == 0) factorial_arr[n] = factorial(n);
-        return factorial_arr[n];
-    }
-
-    // 조합 n C r = n! / ((n-r)! * r!)
-    public int combination(int n, int r) {
-        int a, b, c;
-
-        // n! -> a
-        a = fact(n);
-
-        // (n-1)! -> b
-        b = fact(n-r);
-
-        // r! -> c
-        c = fact(r);
-
-        // System.out.println("n : " + n + " r : " + r);
-        // System.out.println("a : " + a + " b : " + b + " c : " + c);
-        // System.out.println(a/(b*c));
-
-        return a/(b*c);
-    }
-}
