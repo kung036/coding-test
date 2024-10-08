b9acfb9b46a127cd07b37323f29f1ffc16bf2bb3
kung036 <80895384+kung036@users.noreply.github.com>
2024-10-08 14:57:33 +0900

    [level 4] Title: 도둑질, Time: 19.45 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -2,20 +2,14 @@ class Solution {
     public int solution(int[] money) {
         int n = money.length;
 
-        // 첫번째 집을 훔치는 경우
-        int[] dp1 = new int[n];
+        int[] dp1 = new int[n]; // 첫번째 집을 훔치는 경우
         dp1[0] = money[0];
         dp1[1] = money[0];
-        
-        for(int i=2; i<n-1; i++) {
-            dp1[i] = Math.max(dp1[i-1], money[i]+dp1[i-2]);
-        }
-        
-        // 두번째 집을 훔치는 경우
-        int[] dp2 = new int[n];
+        int[] dp2 = new int[n]; // 두번째 집을 훔치는 경우
         dp2[1] = money[1];
         
         for(int i=2; i<n; i++) {
+            if(i != (n-1)) dp1[i] = Math.max(dp1[i-1], money[i]+dp1[i-2]);
             dp2[i] = Math.max(dp2[i-1], money[i]+dp2[i-2]);
         }
         
