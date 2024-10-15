a0977ee020e15e161faf2ccce376d50d48f20371
kung036 <80895384+kung036@users.noreply.github.com>
2024-10-15 13:51:09 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 1030.38 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -3,50 +3,36 @@ import java.util.*;
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        int idx = n - 1;
+        int idx = n-1;
+        int del = 0; // 배달 가능한 택배 개수
+        int pick = 0; // 수거 가능한 택배 개수
         
-        while (idx >= 0) {
-            int del = cap;
-            int pick = cap;
+        while(0<=idx) {
+            while(idx >= 0 && (deliveries[idx] == 0 && pickups[idx] == 0)) idx--;
+            answer += idx+1;
+            del = cap;
+            pick = cap;
             
-            // 가장 멀리 남은 배달 또는 수거의 위치 찾기
-            while (idx >= 0 && deliveries[idx] == 0 && pickups[idx] == 0) {
-                idx--;
-            }
-            
-            // 왕복 거리 더하기
-            if (idx >= 0) answer += (idx + 1) * 2;
-
-            // 현재 배달 처리
-            for (int i = idx; i >= 0; i--) {
+            for(int i=idx; i>=0; i--) {
+               // 배달 처리
                 if (deliveries[i] > 0) {
                     int deliverAmount = Math.min(del, deliveries[i]);
                     deliveries[i] -= deliverAmount;
                     del -= deliverAmount;
                 }
-                
-                // 배달용량 소진 시 반복 종료
-                if (del == 0) break;
+                if(del<=0) break;
             }
-
-            // 현재 수거 처리
-            for (int i = idx; i >= 0; i--) {
+            for(int i=idx; i>=0; i--) {
+                // 수거 처리
                 if (pickups[i] > 0) {
                     int pickupAmount = Math.min(pick, pickups[i]);
                     pickups[i] -= pickupAmount;
                     pick -= pickupAmount;
                 }
-                
-                // 수거용량 소진 시 반복 종료
-                if (pick == 0) break;
+                if(pick<=0) break;
             }
-            
-            // 다음 배달/수거 대상 찾기
-            while (idx >= 0 && deliveries[idx] == 0 && pickups[idx] == 0) {
-                idx--;
-            }
-        }
+        }        
         
-        return answer;
+        return answer*2;
     }
 }
