d624d081689aeeffd0d78215466c57f37046d026
신우경 <80895384+kung036@users.noreply.github.com>
2024-12-28 18:00:38 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.05 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,33 +1,42 @@
 import java.util.*;
 
 class Solution {
-    public boolean dividing(int[] arr, int num){
-        for(int n : arr){
-            if(n%num == 0) continue;
-            else return false;
-        }
-
-        return true;
-    }
-
-    public boolean notDividing(int[] arr, int num){
-        for(int n : arr){
-            if(n%num != 0) continue;
-            else return false;
-        }
-
-        return true;
-    }
-
     public int solution(int[] arrayA, int[] arrayB) {
+        // 최대공약수 구하기
+        int gcdA = gcds(arrayA);
+        int gcdB = gcds(arrayB);
+        
+        // gcd로 다른 배열이 나눠지는지 확인하기
+        boolean validA = isValid(arrayB, gcdA);
+        boolean validB = isValid(arrayA, gcdB);
+        
+        // 정답 구하기
         int answer = 0;
-        int num = Math.max(arrayA[0], arrayB[0]);
-        for(int i=2; i<=num; i++){
-            if((dividing(arrayA, i) && notDividing(arrayB,i)) || 
-              (dividing(arrayB, i) && notDividing(arrayA,i))){
-                answer = i;
-            }
-        }
+        if(validA && validB) answer = Math.max(gcdA, gcdB);
+        else if(validA) answer = gcdA;
+        else if(validB) answer = gcdB;
+        
         return answer;
+    }
+    
+    public int gcd(int num1, int num2) {
+          if (num1 % num2 == 0) return num2;
+          return gcd(num2, num1%num2);
+    }
+    
+    public int gcds(int[] arr) {
+        int gcd = arr[0];
+        for(int i=1; i<arr.length; i++) {
+            gcd = gcd(arr[i], gcd);
+        }
+        return gcd;
+    }
+    
+    public boolean isValid(int[] arr, int gcd) {
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] % gcd == 0) return false;
+        }
+        
+        return true;
     }
 }
