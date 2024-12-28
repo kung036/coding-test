31c4f70d5e0ad6829fe20bce93d21ffa4fb73074
신우경 <80895384+kung036@users.noreply.github.com>
2024-12-28 17:29:54 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.13 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,49 +1,33 @@
 import java.util.*;
 
 class Solution {
+    public boolean dividing(int[] arr, int num){
+        for(int n : arr){
+            if(n%num == 0) continue;
+            else return false;
+        }
+
+        return true;
+    }
+
+    public boolean notDividing(int[] arr, int num){
+        for(int n : arr){
+            if(n%num != 0) continue;
+            else return false;
+        }
+
+        return true;
+    }
+
     public int solution(int[] arrayA, int[] arrayB) {
-        // 배열 A와 B의 최대공약수 구하기
-        int gcdA = getGCD(arrayA);
-        int gcdB = getGCD(arrayB);
-
-        // 상대 배열의 모든 요소를 나눌 수 없는지 확인
-        boolean isValidA = isGCDValid(gcdA, arrayB);
-        boolean isValidB = isGCDValid(gcdB, arrayA);
-
-        // 결과 계산
-        if (isValidA && isValidB) {
-            return Math.max(gcdA, gcdB); // 둘 다 유효하면 더 큰 값 반환
-        } else if (isValidA) {
-            return gcdA; // gcdA만 유효
-        } else if (isValidB) {
-            return gcdB; // gcdB만 유효
-        } else {
-            return 0; // 둘 다 유효하지 않으면 0 반환
-        }
-    }
-
-    // 배열의 최대공약수를 구하는 함수
-    private int getGCD(int[] array) {
-        int gcd = array[0];
-        for (int i = 1; i < array.length; i++) {
-            gcd = GCD(gcd, array[i]);
-        }
-        return gcd;
-    }
-
-    // 유클리드 호제법으로 최대공약수를 구하는 함수
-    private int GCD(int a, int b) {
-        if (b == 0) return a;
-        return GCD(b, a % b);
-    }
-
-    // 최대공약수가 상대 배열의 모든 요소를 나눌 수 없는지 확인하는 함수
-    private boolean isGCDValid(int gcd, int[] array) {
-        for (int num : array) {
-            if (num % gcd == 0) {
-                return false; // 나눌 수 있으면 유효하지 않음
+        int answer = 0;
+        int num = Math.max(arrayA[0], arrayB[0]);
+        for(int i=2; i<=num; i++){
+            if((dividing(arrayA, i) && notDividing(arrayB,i)) || 
+              (dividing(arrayB, i) && notDividing(arrayA,i))){
+                answer = i;
             }
         }
-        return true;
+        return answer;
     }
 }
