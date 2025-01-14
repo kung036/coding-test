5cf56ada1dd6aa82a738569a71d364ad5c34870e
신우경 <80895384+kung036@users.noreply.github.com>
2025-01-14 22:14:22 +0900

    [level 2] Title: 디펜스 게임, Time: 147.50 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -2,29 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int k, int[] enemy) {
-        // 우선순위 큐를 내림차순으로 생성
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        int soldiers = n;
+        Queue<Integer> q = new PriorityQueue<>((o1, o2) -> Integer.compare(o2, o1));
         
-        for (int i = 0; i < enemy.length; i++) {
-            // 현재 라운드의 적을 우선순위 큐에 추가
-            pq.offer(enemy[i]);
-            soldiers -= enemy[i];
+        for(int i=0; i<enemy.length; i++) {
+            q.add(enemy[i]);
+            n -= enemy[i];
             
-            // 병사가 부족한 경우
-            if (soldiers < 0) {
-                // 무적권이 남아있는 경우
-                if (k > 0) {
-                    // 가장 많은 적이 있는 라운드에 무적권 사용
-                    soldiers += pq.poll();
+            // 남은 병사가 없는 경우
+            if(n < 0) {
+                if(k == 0) return i; // 남은 무적권이 없는 경우
+                else { // 무적권이 남은 경우
                     k--;
-                } else {
-                    return i;
+                    n += q.poll();
                 }
             }
-        }
+        }        
         
-        // 모든 라운드를 클리어한 경우
         return enemy.length;
     }
 }
