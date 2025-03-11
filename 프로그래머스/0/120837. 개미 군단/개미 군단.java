fa601f26c9cb27e9e37a15368cce58a315aabf5d
신우경 <80895384+kung036@users.noreply.github.com>
2025-03-11 23:19:37 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        int[] hps = new int[]{5, 3, 1};
+        for(int i=0; i<hps.length; i++) {
+            while(hp >= hps[i]) {
+                answer++;
+                hp -= hps[i];
+            }
+        }
+        
+        return answer;
+    }
+}
