e236510145acbdffb2db726ffb654982edb80ba5
신우경 <80895384+kung036@users.noreply.github.com>
2025-03-22 17:08:20 +0900

    [level 0] Title: 암호 해독, Time: 4.66 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,9 +1,11 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 class Solution {
     public String solution(String cipher, int code) {
-        String answer = "";
-        for(int i=code-1; i<cipher.length(); i+=code) {
-            answer += cipher.charAt(i);
-        }
-        return answer;
+        return IntStream.range(0, cipher.length())
+                .filter(value -> value % code == code - 1)
+                .mapToObj(c -> String.valueOf(cipher.charAt(c)))
+                .collect(Collectors.joining());
     }
 }
