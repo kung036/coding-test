6805c7bf4bedc8c8037fe2a9744d7dc1a1f929b1
신우경 <80895384+kung036@users.noreply.github.com>
2025-04-08 21:41:25 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 3.60 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -1,16 +1,15 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
     public int[] solution(int[] arr, int[] delete_list) {
-        // 삭제 목록을 Set으로 변환 (빠른 검색을 위해)
-        Set<Integer> deleteSet = Arrays.stream(delete_list)
-                                      .boxed()
-                                      .collect(Collectors.toSet());
+        List<Integer> answer = new ArrayList<>();
+        Arrays.sort(delete_list);
+        for(int i : arr) {
+            if(Arrays.binarySearch(delete_list, i) < 0) {
+                answer.add(i);
+            }
+        }
         
-        // 삭제 목록에 없는 원소만 필터링
-        return Arrays.stream(arr)
-                    .filter(num -> !deleteSet.contains(num))
-                    .toArray();
+        return answer.stream().mapToInt(s->s).toArray();
     }
 }
