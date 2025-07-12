b9f1bb308f28c3973535550458f71acbcf6e0d17
신우경 <80895384+kung036@users.noreply.github.com>
2025-07-12 14:25:50 +0900

    [level 0] Title: 배열 만들기 6, Time: 16.57 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -4,10 +4,10 @@ class Solution {
     public int[] solution(int[] arr) {
         Stack<Integer> stack = new Stack<>();
         for(int i : arr) {
-            if(!stack.isEmpty() && stack.peek() == i) stack.pop();
+            if(stack.peek() == i) stack.pop();
             else stack.push(i);
         }
         
-        return stack.isEmpty() ? new int[]{-1} : stack.stream().mapToInt(Integer::intValue).toArray();
+        return stack.isEmpty() ? new int[]{-1} : stack.toArray(new int[0]);
     }
 }
