9165c1200d406b50041b1cdf8a4a7fc38980d2c9
신우경 <80895384+kung036@users.noreply.github.com>
2025-07-15 22:22:19 +0900

    [level 0] Title: 소인수분해, Time: 4.57 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -1,28 +1,19 @@
-import java.util.*;
+import java.util.LinkedHashSet;
 
 class Solution {
     public int[] solution(int n) {
-        List<Integer> list = new ArrayList<>();
-        
-        // 소수 구하기
-        for(int i=2; i<=n; i++) {
-            for(int j=2; j<=i; j++) {
-                if(i%j == 0 && i==j) list.add(i);
-                else if(i%j == 0) break;
+        LinkedHashSet<Integer> primeNumbers = new LinkedHashSet<>();
+
+        int i = 2;
+        while (n != 0 && i <= n) {
+            if (n % i == 0) {
+                primeNumbers.add(i);
+                n /= i;
+            } else {
+                i++;
             }
         }
-        
-        // 소인수분해 구하기
-        Set<Integer> set = new HashSet<>();
-        while(n > 1) {
-            for(int i : list) {
-                if(n%i == 0) {
-                    set.add(i);
-                    n /= i;
-                }
-            }
-        }
-        
-        return set.stream().sorted((o1, o2) -> Integer.compare(o1, o2)).mapToInt(i -> i).toArray();
+
+        return primeNumbers.stream().mapToInt(Integer::intValue).toArray();
     }
 }
