daef5199b1bc691cc7e4a32f6b66b68b1f945e6b
신우경 <80895384+kung036@users.noreply.github.com>
2025-08-12 20:51:03 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,12 +1,8 @@
 class Solution {
     public int solution(int chicken) {
-        int answer = 0; // 서비스 치킨 수
-        int coupons = chicken; // 처음 받는 쿠폰 수 = 주문한 치킨 수
-
-        while (coupons >= 10) {
-            int service = coupons / 10;  // 이번에 받을 서비스 치킨 수
-            answer += service;           // 서비스 치킨 누적
-            coupons = service + (coupons % 10); // 새 쿠폰(서비스 치킨) + 남은 쿠폰
+        int answer = chicken/9;
+        if(chicken>1&&chicken%9==0){
+            answer--;
         }
 
         return answer;
