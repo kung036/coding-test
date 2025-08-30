65bcbef1a9848b76eef7d07f8c4dfb20b35f0c1a
신우경 <80895384+kung036@users.noreply.github.com>
2025-08-30 20:27:27 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        int d = common[1] - common[0];
+        int n = common.length;
+        
+        if(common[2] - common[1] == d) { // 등차수열
+            answer = common[0] + n*d;
+        } else { // 등비수열
+            answer = common[n-1] * (common[1] / common[0]);
+        }
+        
+        return answer;
+    }
+}
