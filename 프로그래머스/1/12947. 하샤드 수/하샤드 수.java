696bc4446c35071939432c443aa5ea61204a92c0
신우경 <80895384+kung036@users.noreply.github.com>
2025-10-04 20:54:47 +0900

    [level 1] Title: 하샤드 수, Time: 2.41 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,9 +1,6 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = false;
-        int sum = new StringBuilder(String.valueOf(x)).chars().map(Character::getNumericValue).sum();
-        if(x%sum == 0) answer = true;
-        
-        return answer;
+        int sum = String.valueOf(x).chars().map(ch -> ch - '0').sum();
+        return x % sum == 0;
     }
 }
