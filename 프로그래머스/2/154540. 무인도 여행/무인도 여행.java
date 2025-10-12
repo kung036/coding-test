cdf0a7c7fb9d177caa16b8f0d10679506f9ade22
신우경 <80895384+kung036@users.noreply.github.com>
2025-10-12 11:47:10 +0900

    [level 2] Title: 무인도 여행, Time: 8.07 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -2,49 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] maps) {
-        // X : 바다, 숫자 : 무인도(서로 연결됨) -> 머물 수 있는 날짜
-        // 각 섬에서 최대 며칠씩 머무를 수 있는지 배열에 오름차순
-        // 없으면 -1 반환
-        
-        // bfs로 서로 연결된 무인도 연결하기
-        int row = maps.length; // 세로 길이
-        int column = maps[0].length(); // 가로 길이
-        int sum = 0; // 각 섬의 채류 가능 일수
-        boolean[][] visited = new boolean[row][column];
-        List<Integer> answer = new ArrayList<>();
-        Queue<int[]> q = new ArrayDeque<>();
-        int[] dy = {-1, 1, 0, 0};
-        int[] dx = {0, 0, -1, 1};
-        
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<column; j++) {
-                // 이미 방문하거나 바다면 건너띄기
-                if(visited[i][j] || maps[i].charAt(j) == 'X') continue;
-                
-                q.add(new int[]{i,j});
-                sum = 0;
-                while(!q.isEmpty()) {
-                    int y = q.peek()[0];
-                    int x = q.poll()[1];
-                    if(visited[y][x]) continue;
-                    visited[y][x] = true;
-                    sum += Integer.valueOf(maps[y].charAt(x)+"");
+        boolean[][] visit = new boolean[maps.length][maps[0].length()];
+        List<Integer> list = new ArrayList<>();
 
-                    // 상하좌우 확인 -> 연결되고 방문되지 않은 무인도 확인
-                    for(int k=0; k<4; k++) {
-                        int ny = y + dy[k];
-                        int nx = x + dx[k];
-                        if(ny<row && 0<=ny && nx<column && 0<=nx) {
-                            if(!visited[ny][nx] && maps[ny].charAt(nx) != 'X')
-                                q.add(new int[]{ny, nx});
-                        }
-                    }
+        for(int i=0 ; i<maps.length ; i++) {
+            int sum = 0;
+            for(int j=0 ; j<maps[i].length() ; j++) {
+                sum = getTerritory(i, j, visit, maps);
+                if(sum>0) {
+                    list.add(sum);
                 }
-                answer.add(sum);
             }
         }
-        
-        if(answer.isEmpty()) return new int[]{-1};
-        return answer.stream().mapToInt(Integer::intValue).sorted().toArray();
+        Collections.sort(list);
+        return list.isEmpty()?new int[]{-1}:list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int getTerritory(int i, int j, boolean[][] visit, String[] maps) {
+        if(i<0 || j<0 || i>=visit.length || j>=visit[0].length || visit[i][j] || maps[i].charAt(j)=='X') {
+            return 0;
+        }
+        visit[i][j] = true;
+        return (maps[i].charAt(j)-'0')
+            + getTerritory(i-1,j,visit,maps)
+            + getTerritory(i+1,j,visit,maps)
+            + getTerritory(i,j-1,visit,maps)
+            + getTerritory(i,j+1,visit,maps);
     }
 }
