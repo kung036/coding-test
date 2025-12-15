93ba2b10fe39ab254f9668a4ddf1b7b2c6755f73
신우경 <80895384+kung036@users.noreply.github.com>
2025-12-15 21:34:52 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,25 @@
+-- MySQL
+SELECT  A.SCORE,
+        E.EMP_NO,
+        E.EMP_NAME,
+        E.POSITION,
+        E.EMAIL
+FROM (
+    SELECT  EMP_NO,
+            SUM(SCORE) AS SCORE
+    FROM HR_GRADE
+    WHERE YEAR = '2022'
+    GROUP BY EMP_NO
+) A
+JOIN HR_EMPLOYEES E
+  ON A.EMP_NO = E.EMP_NO
+WHERE A.SCORE = (
+    SELECT MAX(SCORE)
+    FROM (
+        SELECT  EMP_NO,
+                SUM(SCORE) AS SCORE
+        FROM HR_GRADE
+        WHERE YEAR = '2022'
+        GROUP BY EMP_NO
+    ) X
+);
