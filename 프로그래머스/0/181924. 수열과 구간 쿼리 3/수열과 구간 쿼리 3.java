5b4dfbeba7aa9596bd513804a9cc2a21a6cfe79b
신우경 <80895384+kung036@users.noreply.github.com>
2026-02-03 21:07:22 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.10 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -5,7 +5,6 @@ class Solution {
             tmp = arr[queries[i][0]];
             arr[queries[i][0]] = arr[queries[i][1]];
             arr[queries[i][1]] = tmp;
-            
         }
         return arr;
     }
