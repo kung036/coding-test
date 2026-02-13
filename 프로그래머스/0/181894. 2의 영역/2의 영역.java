f237fca4e36b8c39529d1bd1d2ebef49114a80f3
신우경 <80895384+kung036@users.noreply.github.com>
2026-02-13 21:13:01 +0900

    [level 0] Title: 2의 영역, Time: 24.82 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -4,8 +4,8 @@ class Solution {
     public int[] solution(int[] arr) {
         List<Integer> list = new ArrayList<>();
         int n = (int) Arrays.stream(arr).filter(i->i==2).count();
-        if(n == 0) return new int[]{-1};
-        if(n == 1) return new int[]{2};
+        if(n == 0) return new int[]{ -1 };
+        if(n == 1) return new int[]{ 2 };
         
         int x = 0;
         for(int i : arr) {
