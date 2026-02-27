4bc35b6bcb868240cc51642af4f97c3ed45ae996
신우경 <80895384+kung036@users.noreply.github.com>
2026-02-27 21:50:23 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/README.md
+++ 프로그래머스/0/120924. 다음에 올 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 0] 다음에 올 숫자 - 120924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120924#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120924) 
 
 ### 성능 요약
 
-메모리: 75 MB, 시간: 0.03 ms
+메모리: 79.9 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 30일 20:27:24
+2026년 02월 27일 21:50:18
 
 ### 문제 설명
 
--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -4,9 +4,11 @@ class Solution {
         int d = common[1] - common[0];
         int n = common.length;
         
-        if(common[2] - common[1] == d) { // 등차수열
+        if(common[2] - common[1] == d) {
+            // 등차수열
             answer = common[0] + n*d;
-        } else { // 등비수열
+        } else { 
+            // 등비수열
             answer = common[n-1] * (common[1] / common[0]);
         }
         
