2a2db3adb5ea1e8111faccc7e010bb648e074ff2
신우경 <80895384+kung036@users.noreply.github.com>
2026-03-05 20:46:13 +0900

    [level 3] Title: 네트워크, Time: 1.64 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 78.1 MB, 시간: 0.55 ms
+메모리: 97.9 MB, 시간: 1.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 13일 13:02:50
+2026년 03월 05일 20:46:09
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,21 +1,22 @@
 import java.util.*;
 
+// computers : 인접 행렬로 표현된 그래프
+// dfs를 통해서 network
 class Solution {
-    public int solution(int n, int[][] computers) {
-        // computers : 인접 행렬로 표현된 그래프
-        // dfs를 통해서 network
-        
+    public int solution(int n, int[][] computers) {        
         int answer = 0;
         Set<Integer> network = new HashSet<>(); // 네트워크로 연결된 컴퓨터
         Stack<Integer> stack = new Stack<>();
         stack.push(0);
         
         for(int i=0; i<n; i++) {
-            if(network.contains(i)) continue; // 방문하지 않은 노드만 순회
+            // 방문하지 않은 노드만 순회
+            if(network.contains(i)) continue;
             answer++;
             stack.add(i);
             
-            while(!stack.isEmpty()) { // 연결된 네트워크 전부 순회
+            // 연결된 네트워크 전부 순회
+            while(!stack.isEmpty()) {
                 int computer = stack.pop();
                 network.add(computer);
 
