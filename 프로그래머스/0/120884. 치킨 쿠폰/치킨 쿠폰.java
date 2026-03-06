a66365c4e00576bdb7dcb57717cd464ff0844d13
신우경 <80895384+kung036@users.noreply.github.com>
2026-03-06 22:25:06 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,10 +1,7 @@
 class Solution {
     public int solution(int chicken) {
-        int answer = chicken/9;
-        if(chicken>1&&chicken%9==0){
-            answer--;
-        }
-
+        int answer = chicken / 9;
+        if(chicken > 1 && chicken%9 == 0) answer--;
         return answer;
     }
 }
