42c0a177fd1a3c9a187609451f1e5e23ba175d8c
신우경 <80895384+kung036@users.noreply.github.com>
2026-03-09 20:19:44 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 112.73 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -6,15 +6,18 @@ class Solution {
         set.add(Arrays.stream(elements).sum());
         int n = elements.length;
         
-        for(int i=1; i<n; i++) { // 연속 부분 수열의 길이
+        // 시작 인덱스
+        for(int i=1; i<n; i++) {
             int sum = 0;
+            
             // i-1 길이만큼의 합 구하기
             for(int j=0; j<i; j++) {
                 sum += elements[j];
             }
             set.add(sum);
             
-            for(int j=0; j<n; j++) { // 시작 인덱스
+            // 시작 인덱스
+            for(int j=0; j<n; j++) {
                 sum -= elements[j];
                 sum += elements[(j+i)%n];
                 set.add(sum);
