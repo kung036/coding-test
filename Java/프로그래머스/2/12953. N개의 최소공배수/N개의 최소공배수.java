8091cf916fa422364a30cf65257e8ab293ddd86d
신우경 <80895384+kung036@users.noreply.github.com>
2026-03-17 23:01:37 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.51 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr) {
+        Arrays.sort(arr); // 오름차순 정렬
+        int answer = arr[0]; // 최소공배수
+        
+        for(int i=1; i<arr.length; i++) {
+            int a = Math.max(answer, arr[i]);
+            int b = Math.min(answer, arr[i]);
+            answer = (a*b) / gcd(a, b);
+        }
+        
+        return answer;
+    }
+    
+    // 최소공배수 구하기(a > b)
+    public int gcd(int a, int b) {
+        if(b == 0) return a;
+        return gcd(b, a%b);
+    }
+}
