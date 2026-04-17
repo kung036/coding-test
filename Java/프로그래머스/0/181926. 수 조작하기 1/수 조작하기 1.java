c1916636d3772d7fac442d5e9dd8ddc9603031f6
신우경 <80895384+kung036@users.noreply.github.com>
2026-04-17 23:25:53 +0900

    [level 0] Title: 수 조작하기 1, Time: 18.89 ms, Memory: 86.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ Java/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,15 +1,14 @@
+import java.util.Map;
+
 class Solution {
     public int solution(int n, String control) {
         int answer = n;
-        for(char c : control.toCharArray()) {
-            switch(c) {
-                case 'w' -> answer++;
-                case 's' -> answer--;
-                case 'd' -> answer+=10;
-                case 'a' -> answer-=10;
-            }
+        Map<Character, Integer> moveMap = Map.of('w', 1, 's', -1, 'd', 10, 'a', -10);
+
+        for (char c : control.toCharArray()) {
+            answer += moveMap.getOrDefault(c, 0);
         }
-        
+
         return answer;
     }
 }
