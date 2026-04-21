b80f63a611c9512cc36e67202956f71a831662b7
신우경 <80895384+kung036@users.noreply.github.com>
2026-04-21 22:01:23 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 3.01 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int sum = Arrays.stream(num_list).sum();
+        return sum * sum < Arrays.stream(num_list)
+            .reduce((x,y)-> x*y)
+            .getAsInt() ? 0 : 1;
+    }
+}
