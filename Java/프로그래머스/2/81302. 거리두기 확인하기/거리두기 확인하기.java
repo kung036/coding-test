7b5104b1829ea1caefa753ae43433f54f851a97b
신우경 <80895384+kung036@users.noreply.github.com>
2026-05-22 22:00:37 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.07 ms, Memory: 61.7 MB -BaekjoonHub

--- Java/프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ Java/프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,39 +1,53 @@
 class Solution {
 
-    private static int[] dx = {0, 0, 1, -1};
-    private static int[] dy = {1, -1, 0, 0};
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, -1, 1};
 
     public int[] solution(String[][] places) {
-        int[] answer = {1, 1, 1, 1, 1};
+        int[] answer = new int[places.length];
 
-        for (int t = 0; t < places.length; t++) {
-            char[][] place = new char[5][5];
-            for (int i = 0; i < 5; i++) {
-                place[i] = places[t][i].toCharArray();
-            }
-
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-                    if (place[i][j] == 'P') {
-                        for (int k = 0; k < 4; k++) {
-                            int x = i + dx[k];
-                            int y = j + dy[k];
-
-                            if (x < 0 || x > 4 || y < 0 || y > 4)
-                                continue;
-
-                            if (place[x][y] == 'P' || place[x][y] == 'p')
-                                answer[t] = 0;
-
-                            if (place[x][y] == 'O')
-                                place[x][y] = 'p';
-                        }
-                    }
-                }
-            }
-
+        for (int i = 0; i < places.length; i++) {
+            answer[i] = isValidPlace(places[i]) ? 1 : 0;
         }
 
         return answer;
+    }
+
+    private boolean isValidPlace(String[] place) {
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 5; y++) {
+                if (place[x].charAt(y) == 'P' && !checkFrom(place, x, y)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean checkFrom(String[] place, int x, int y) {
+        boolean[][] visited = new boolean[5][5];
+        return dfs(place, x, y, x, y, 0, visited);
+    }
+
+    private boolean dfs(String[] place, int sx, int sy, int x, int y, int dist, boolean[][] visited) {
+        if (dist > 2) return true;
+        if (dist > 0 && place[x].charAt(y) == 'P') return false;
+        if (place[x].charAt(y) == 'X') return true;
+
+        visited[x][y] = true;
+
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+
+            if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
+            if (visited[nx][ny]) continue;
+
+            if (!dfs(place, sx, sy, nx, ny, dist + 1, visited)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
