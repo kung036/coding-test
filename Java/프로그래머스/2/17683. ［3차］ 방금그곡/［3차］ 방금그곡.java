bb986c1f7d11a370a66ac1ada2129bd91193142a
신우경 <80895384+kung036@users.noreply.github.com>
2026-05-25 16:12:18 +0900

    [level 2] Title: [3차] 방금그곡, Time: 5.17 ms, Memory: 59.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String solution(String m, String[] musicinfos) {
+        String answer = "(None)";
+        int time = 0;
+
+        m = edit(m);
+
+        for (int inx = 0; inx < musicinfos.length; inx++) {
+
+            String[] info = musicinfos[inx].split(",");
+
+            int start = (60 * Integer.parseInt(info[0].substring(0, 2)) + Integer.parseInt(info[0].substring(3)));
+            int end = (60 * Integer.parseInt(info[1].substring(0, 2)) + Integer.parseInt(info[1].substring(3)));
+            int t = end - start;
+
+            if (t > time) {
+                info[3] = edit(info[3]);
+                StringBuffer sb = new StringBuffer();
+                for (int jnx = 0; jnx < t; jnx++) {
+                    sb.append(info[3].charAt(jnx % (info[3].length())));
+                }
+                if (sb.toString().indexOf(m) >= 0) {
+                    answer = info[2];
+                    time = t;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public String edit(String m) {
+        m = m.replace("C#", "V")
+             .replace("D#", "W")
+             .replace("F#", "X")
+             .replace("G#", "Y")
+             .replace("A#", "Z")
+             .replace("B#", "U");
+        return m;
+    }
+}
