e303571e0306f1f8387f9e19b55fd55791682b44
신우경 <80895384+kung036@users.noreply.github.com>
2026-09-14 21:43:51 +0900

    [level 3] Title: 거스름돈, Time: 14.25 ms, Memory: 57.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        int MOD = 1_000_000_007;
+        
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        
+        for (int coin : money) {
+            for (int i = coin; i <= n; i++) {
+                dp[i] = (dp[i] + dp[i - coin]) % MOD;
+            }
+        }
+        
+        return (int) dp[n];
+    }
+}
