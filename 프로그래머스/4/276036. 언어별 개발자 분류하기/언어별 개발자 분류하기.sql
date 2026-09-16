01ab732ca7581f6c004e1d358a3ffb6c4ddf0a34
신우경 <80895384+kung036@users.noreply.github.com>
2026-09-16 20:28:43 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -0,0 +1,20 @@
+-- MySQL
+SELECT GRADE, ID, EMAIL
+FROM (
+    SELECT
+        ID,
+        EMAIL,
+        CASE
+            WHEN (SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End')) > 0
+                 AND (SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python')) > 0
+            THEN 'A'
+            WHEN (SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#')) > 0
+            THEN 'B'
+            WHEN (SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End')) > 0
+            THEN 'C'
+            ELSE NULL
+        END AS GRADE
+    FROM DEVELOPERS
+) T
+WHERE GRADE IS NOT NULL
+ORDER BY GRADE, ID;
